a6d711f7140b804a64d3e8b54b6418b662df409d
chrisMasoud <chrismasoud22@gmail.com>
2023-02-18 14:58:00 -0500

    Completed implementation of the Executive pay() method

--- src/main/java/com/mycompany/agency/Executive.java
+++ src/main/java/com/mycompany/agency/Executive.java
@@ -32,6 +32,6 @@ public class Executive extends StaffEmployee
     //-----------------------------------------------------------------
     public double pay()
     {
-       
+       return this.payRate + this.bonus;
     }
 }
