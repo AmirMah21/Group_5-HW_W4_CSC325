5d3f7e91dfb1679e42f9117f5e36035b14df4abf
TLuu52 <luuthuan817@gmail.com>
2023-02-19 11:40:10 -0500

    Added the staff to Staff.java

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -1,43 +1,50 @@
 package com.mycompany.agency;
 
+public class Staff {
 
-
-
-public class Staff
-{
     private StaffMember[] staffList;
 
     //-----------------------------------------------------------------
     //  Constructor: Sets up the list of staff members.
     //-----------------------------------------------------------------
-    public Staff()
-    {
+    public Staff() {
         staffList = new StaffMember[6];
 
         // TODO 1: Add 6 emploees to the staffList
+        ((Executive) staffList[0]).awardBonus(500.00);
+        ((TempEmploee) staffList[3]).addHours(40);
 
-        ((Executive)staffList[0]).awardBonus(500.00);
-
-        ((TempEmploee)staffList[3]).addHours(40);
+        ((Executive) staffList[0]).awardBonus(500.00);
+        ((TempEmployee) staffList[3]).addHours(40);
+        staffList[0] = new Executive("Tim", "AAA", "111-111-1111", "111-11-1111", 5.00);
+        staffList[1] = new StaffEmployee("Bob", "AAA", "222-222-2222", "222-22-2222", 3.00);
+        staffList[2] = new StaffEmployee("Rain", "AAA", "333-333-3333", "333-33-3333", 4.00);
+        staffList[3] = new StaffEmployee("Steven", "AAA", "444-444-4444", "444-44-4444", 12.60);
+        staffList[4] = new TempEmployee("Jim", "AAA", "555-555-5555", "555-55-5555", 1.25);
+        staffList[5] = new Intern("Sue", "AAA", "666-666-6666", "666-66-6666", 0.50);
+        ((StaffEmployee) staffList[1]).pay();
+        ((StaffEmployee) staffList[2]).pay();
+        ((StaffEmployee) staffList[3]).pay();
+        ((TempEmployee) staffList[4]).addHours(40);
+        ((Intern) staffList[5]).pay();
     }
 
     //-----------------------------------------------------------------
     //  Pays all staff members.
     //-----------------------------------------------------------------
-    public void payday ()
-    {
+    public void payday() {
         double amount;
 
-        for (int count=0; count < staffList.length; count++)
-        {
+        for (int count = 0; count < staffList.length; count++) {
             System.out.println(staffList[count]);
 
             amount = staffList[count].pay();  // polymorphic
 
-            if (amount == 0.0)
+            if (amount == 0.0) {
                 System.out.println("Thanks!");
-            else
+            } else {
                 System.out.println("Paid: " + amount);
+            }
 
             System.out.println("-----------------------------------");
         }
